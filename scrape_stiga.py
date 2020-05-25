8cd1054223463076883acff1d417a1b42ad218d9
jankomag <38592985+jankomag@users.noreply.github.com>
2020-05-25 10:12:48 +0200

    Update scrape_stiga.py

--- scrape_stiga.py
+++ scrape_stiga.py
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import urllib
-import urllib.request
-from bs4 import BeautifulSoup
-
-i = 1
-
-def makesoup(url):
-     thepage = urllib.request.urlopen(url)
-     soupdata = BeautifulSoup(thepage, "html.parser")
-     return soupdata
-
-soup = makesoup("https://www.stiga.pl/sklep/koszenie-trawnika/agregaty-tnace/agregaty-tnace-park-villa/agregat-park-100-combi-3-el-qf")
-for img in soup.findAll('img'):
-    temp=img.get('src')
-    if temp[:1]=="/":
-        image = "https://www.stiga.pl/sklep/koszenie-trawnika/agregaty-tnace/agregaty-tnace-park-villa/agregat-park-100-combi-3-el-qf" + temp
-    else:
-        image = temp
-
-    nametemp = img.get('alt','')
-    if len(nametemp) == 0:
-        filename = str(i)
-        i = i + 1
-    else: 
-        filename = nametemp
-
-    imagefile = open(filename + ".jpeg", "wb")
-    imagefile.write(urllib .request.urlopen(image).read())
-    imagefile.close()
-=======
 def download_image(image_url):
     import requests
     from pathlib import Path
